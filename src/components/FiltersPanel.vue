214935820ff62b2cc3c5906773d4a124762f53c6
he1snber8 <116442582+he1snber8@users.noreply.github.com>
2025-11-17 16:44:34 +0400

    Add FiltersPanel.vue

---
+++ src/components/FiltersPanel.vue
@@ -0,0 +1,12 @@
+<template>
+  <div class="filters">
+    <label><input type="checkbox" v-model="vegan" @change="emitFilter"> Vegan</label>
+    <label><input type="checkbox" v-model="glutenFree" @change="emitFilter"> Gluten-free</label>
+  </div>
+</template>
+<script>
+export default {
+  data(){return{vegan:false,glutenFree:false}},
+  methods:{emitFilter(){this.$emit('filter',{vegan:this.vegan,glutenFree:this.glutenFree})}}
+}
+</script>
