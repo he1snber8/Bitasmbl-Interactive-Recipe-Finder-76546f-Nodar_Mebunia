74fe5f4bf4d7bbb0f7887a65d1c1de42efbe995d
he1snber8 <116442582+he1snber8@users.noreply.github.com>
2025-11-17 16:44:31 +0400

    Add SearchBar.vue

---
+++ src/components/SearchBar.vue
@@ -0,0 +1,11 @@
+<template>
+  <div class="search">
+    <input v-model="q" @input="onInput" placeholder="Search recipes or ingredients" />
+  </div>
+</template>
+<script>
+export default {
+  data(){return{q:''}},
+  methods:{onInput(){this.$emit('search',this.q)}}
+}
+</script>
