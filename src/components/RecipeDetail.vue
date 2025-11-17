738837d4b684813585f52ea2efdf7a8f9ff6f2dc
he1snber8 <116442582+he1snber8@users.noreply.github.com>
2025-11-17 16:44:32 +0400

    Add RecipeDetail.vue

---
+++ src/components/RecipeDetail.vue
@@ -0,0 +1,12 @@
+<template>
+  <aside v-if="recipe" class="detail">
+    <h2>{{ recipe.title }}</h2>
+    <h3>Ingredients</h3>
+    <ul><li v-for="ing in recipe.ingredients" :key="ing">{{ing}}</li></ul>
+    <h3>Steps</h3>
+    <ol><li v-for="(s,i) in recipe.steps" :key="i">{{s}}</li></ol>
+  </aside>
+</template>
+<script>
+export default { props:['recipe'] }
+</script>
