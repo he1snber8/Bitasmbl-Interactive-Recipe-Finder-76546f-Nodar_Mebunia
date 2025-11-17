492aa0b2ee85077f58c6ba57d2a386c2de1ce901
he1snber8 <116442582+he1snber8@users.noreply.github.com>
2025-11-17 16:44:32 +0400

    Add RecipeList.vue

---
+++ src/components/RecipeList.vue
@@ -0,0 +1,12 @@
+<template>
+  <section class="list">
+    <ul>
+      <li v-for="r in recipes" :key="r.id">
+        <button @click="$emit('select',r)">{{ r.title }}</button>
+      </li>
+    </ul>
+  </section>
+</template>
+<script>
+export default { props:['recipes'] }
+</script>
